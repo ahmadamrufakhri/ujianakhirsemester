d8f1da81145cb5e19156ca30e36fd4053200304f
amru fakhri <81955084+ahmadamrufakhri@users.noreply.github.com>
2022-01-26 22:15:36 +0700

    Add files via upload

---
+++ Nomor1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+	string nama;
+	string nim;
+	
+	cout<<"Masukan Data Berikut : "<<endl<<endl;
+	cout<<"Nama    : ";
+	getline (cin, nama); 
+	  
+	 
+	cout<<"NIM     : ";
+	getline (cin, nim); 
+	 
+	
+	cout<<"Nama    : "<<nama<<endl;
+	cout<<"NIM     : "<<nim<<endl;
+
+}
