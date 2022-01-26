c6af7e649baa2adc3c9344acedcabc2de8773708
amru fakhri <81955084+ahmadamrufakhri@users.noreply.github.com>
2022-01-27 01:11:47 +0700

    Add files via upload

---
+++ Nomor2.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#include <conio.h>
+#include <string.h>
+
+void Nama(string nama){
+    cout << "Ahmad Amru Fakhri" << nama << endl;
+}
+
+void Nim(string nim){
+    cout << "20200801075" << nim << endl;
+}
+
+int main(){
+    Nama(nama);
+    Nim(nim);
+}
